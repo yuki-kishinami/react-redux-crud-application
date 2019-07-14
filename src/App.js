d8c1959c2e2222bc73e78bb2720b9c8ae31532c6
yuki-kishinami <yuki.kishinami@fullspeedtechnologies.com>
2019-07-14 18:48:02 +0800

    learn state and setState

--- src/App.js
+++ src/App.js
@@ -1,29 +1,28 @@
-import React from 'react';
-import { pipelineTopicExpression } from '@babel/types';
+import React,{Component} from 'react';
 
-function App() {
-  const profiles = [
-    {name: "Taro", age:10},
-    {name: "Hanako", age:5},
-    {name: "Noname"}
+const App = () => (<Counter></Counter>)
 
-  ]
-  return ( 
-    <div>
-      {
-        profiles.map((profiles,index) => {
-          return <User name={profiles.name} age={profiles.age} key={index}/>
-        })
-      }
-    </div>
+class Counter extends Component {
+  constructor(props){
+    super(props)
+    this.state = {count: 0}
+  }
+
+  handlePlusButton = () => {
+    this.setState({count: this.state.count +1})
+  }
+  handleMinusButton = () => {
+    this.setState({count: this.state.count -1})
+  }
+
+  render(){
+    return (
+      <React.Fragment>
+        <div>counter:{this.state.count}</div>
+        <button onClick={this.handlePlusButton}>+1</button>
+        <button onClick={this.handleMinusButton}>-1</button>
+      </React.Fragment>
     )
-}
-
-const User = (props) => {
-  return <div>Hi, I am {props.name}, and {props.age} years old!</div>
-}
-
-User.defaultProps = {
-  age: 1
+  }
 }
 export default App;
