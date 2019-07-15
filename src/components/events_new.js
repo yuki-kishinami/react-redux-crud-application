c5fdddba0e00cb739a4f78f23cb44ffa06c337d1
yuki-kishinami <yuki.kishinami@fullspeedtechnologies.com>
2019-07-15 13:59:03 +0800

    implement event new component

---
+++ src/components/events_new.js
@@ -0,0 +1,18 @@
+import React,{Component} from 'react';
+import { connect } from 'react-redux'
+import {Link} from 'react-router-dom'
+
+// import {postEvents} from '../actions'
+
+class EventsNew extends Component {
+  render(){
+    return (
+      <React.Fragment>
+      </React.Fragment>
+    )
+  }
+}
+
+// const mapDispatchToProps = ({ postEvents })
+
+export default connect(null, null)(EventsNew)
